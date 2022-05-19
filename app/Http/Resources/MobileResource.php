6dfc32f56d9994016f78a13bb1b03458e03506f5
svelezp9 <73863024+svelezp9@users.noreply.github.com>
2022-05-18 20:38:34 -0500

    Update MobileResource.php

--- app/Http/Resources/MobileResource.php
+++ app/Http/Resources/MobileResource.php
@@ -19,7 +19,7 @@ class MobileResource extends JsonResource
             'price' => $this->getPrice(),
             'brand' => $this->getBrand(),
             'model' => $this->getModel(),
-            'linkToMobile' => 'http://127.0.0.1/mobiles/'.$this->getId(),
+            'linkToMobile' => 'http://www.teismobilestore.tk/public/api/mobiles'.$this->getId(),
         ];
     }
 }
